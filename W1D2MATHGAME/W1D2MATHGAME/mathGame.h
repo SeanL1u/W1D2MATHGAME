756337bd233640a94a4d0dd05a3326270c355922
Unknown <seanyuxuan.liu@mail.utoronto.ca>
2017-11-01 14:56:43 -0400

    wed 2pm no questionfactory

--- W1D2MATHGAME/W1D2MATHGAME/mathGame.h
+++ W1D2MATHGAME/W1D2MATHGAME/mathGame.h
@@ -5,6 +5,8 @@
 @property NSString* currentQuestion;
 @property NSArray <NSArray*>* questionArray;
 @property BOOL game;
+@property NSDate* start_time;
+@property NSDate* end_time;
 +(void)start;
 -(NSArray*)questionGenerator;
 -(void)storyloop;
--- W1D2MATHGAME/W1D2MATHGAME/mathGame.m
+++ W1D2MATHGAME/W1D2MATHGAME/mathGame.m
@@ -3,42 +3,73 @@
 NSInteger numberize(NSString* whatever){
     NSInteger outputnum = [whatever integerValue];
     return outputnum;}
-//////////////////////////////////////
 
 @implementation mathGame
 -(instancetype)init {self = [super init];return self;}
 
 -(NSArray*)questionGenerator {
+    self.start_time = [NSDate date];
+    NSArray* operators = @[@"+",@"-",@"x",@"/"];
     int num1 = arc4random() % 11;
     int num2 = arc4random() % 11;
-    int sum = num1 + num2 ;
+    int result = 0;
+    int operator_index = arc4random() % 3;
+    NSString*operator = operators[operator_index];
+    
+    switch (operator_index) {
+        case 0:
+            result = num1 + num2;
+            break;
+        case 1:
+            result = num1 - num2;
+            break;
+        case 2:
+            result = num1 * num2;
+            break;
+        case 3:
+            result = num1 / num2;
+            break;
+        default:
+            break;
+    }
     NSString* num1S = [NSString stringWithFormat: @"%d", num1];
     NSString* num2S = [NSString stringWithFormat: @"%d", num2];
-    NSString* sumS = [NSString stringWithFormat: @"%d", sum];
-    NSLog(@"Anwser : %@", sumS);
-    return @[num1S,num2S,sumS];}
+    NSString* resultS = [NSString stringWithFormat: @"%d", result];
+    NSLog(@"Anwser : %@", resultS);
+    return @[num1S,num2S,resultS,operator];}
 
+//------------------------------------------------
 -(NSString*) getandConvert{
     char inputstr[4];
     scanf("%s", inputstr);
-     if(strcmp(inputstr,"quit\n") != (0)){ self.game = NO;}
-    NSString* whatever = [NSString stringWithUTF8String:inputstr]; return whatever;}
+     if(strcmp(inputstr,"quit") == (0)){self.game = NO;
+         self.end_time = [NSDate date];
+         NSTimeInterval interval = [self.end_time timeIntervalSinceDate:self.start_time];
+         NSLog(@"Time used: %f\nTotal Score %ld",interval,(long)self.score);}
+    
+    NSString* whatever = [NSString stringWithUTF8String:inputstr];
+    return whatever;}
+
 -(void)storyloop{
     while (self.game){
         NSArray* questionGenerated = [self questionGenerator ];
         NSString* num1S = questionGenerated [0];
         NSString* num2S = questionGenerated [1];
-        NSInteger sum = numberize(questionGenerated [2]);
-        NSLog( (@"%@ + %@ = ?") ,(num1S), num2S);
+        NSString* operator = questionGenerated [3];
+        NSInteger result = numberize(questionGenerated [2]);
+        
+        NSLog( (@"%@ %@ %@ = ?") ,(num1S),operator,num2S);
         NSString* inputanwserS = (self.getandConvert);
         NSInteger inputanwser = numberize(inputanwserS);
         //NSLog(@"%hhd",self.game);
         
-        if (inputanwser == sum){
+        if (inputanwser == result){
             self.score += 1;
             NSLog(@"CORRECT!\n");}
-        else {NSLog(@"Wrong :(\n");}
-        NSLog(@"YOUR SCORE IS %ld",(long)self.score);}}
+        else if ([inputanwserS isEqualToString: @"quit"] != YES){
+            NSLog(@"Wrong :(\n");}}
+    
+        NSLog(@"YOUR SCORE IS %ld",(long)self.score);}
 
 +(void)start{ mathGame* Player1 = [[mathGame alloc] init];
     Player1.score = 0;
